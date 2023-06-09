8986ea9c0d5b1ee9edc8e0533d7da880f0ec7208
Nishit Ranjan <nranjan.2004@gmail.com>
2023-06-09 11:46:05 +0530

    object encapsulation, getters and setters, constructors

---
+++ src/main/java/objects/Encapsulation.java
@@ -0,0 +1,55 @@
+package objects;
+
+public class Encapsulation {
+
+    private int length;
+    private int breadth;
+    private int sides =4;
+
+    //below is a constructor
+    //initial values being set
+    public Rectangle(){
+        setLength(0);
+        setWidth(0);
+    }
+
+    //below is a constructor
+    //initial values are set to those that gets passed
+    public Rectangle(int length, int breadth){
+        setLength(length);
+        setBreadth(breadth);
+    }
+
+    public  int getPerimeter(int length, int breadth){
+        return 2*(length+breadth);
+    }
+
+    public  int getArea(int length, int breadth){
+        return length*breadth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+
+    public void setBreadth(int breadth) {
+        this.breadth = breadth;
+    }
+
+    public int getSides() {
+        return sides;
+    }
+
+    public void setSides(int sides) {
+        this.sides = sides;
+    }
+}
